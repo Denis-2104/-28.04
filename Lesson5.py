38fc55fc51604cc7d89f65eecae458949e9b5670
Denis-2104 <gavrilinyandex@gmail.com>
2024-06-04 15:26:46 +0200

    Add files via upload

---
+++ Lesson5.py
@@ -0,0 +1,15 @@
+#name = input ("Введите ваше имя")
+#current_year = 2024
+#date_of_birth = int(input("В_каком_году_вы_родились?")
+#age = current_year - (date_of_birth)
+#print ("Здравствуйте,",name)
+#print ("В этом году вам" ,age, "Лет" )
+#print ('привет, я строка в нижнем регистре'. upper().lower() )
+#print ('привет, я строка в нижнем регистре'. replace("","" '#'))
+#my_string= input("Добрый день, хорошего дня" )
+#letter_width = 1 
+#line_length= int(input ("Длинна строки ровна?" ))
+#print ( "Добрый день, хорошего дня,", my_string)
+#print ("Длинна строки " ,letter_width, "мм" )
+#print  ("Самый хороший день ".upper().lower())
+print ('Самый лучший день'.replace ('Cамый','День'))
