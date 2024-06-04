ef2d3d73f57ec88a1ef208eb4ff7cbe4e72c8bdb
Denis-2104 <gavrilinyandex@gmail.com>
2024-06-04 09:54:35 +0200

    Add files via upload

---
+++ Lesson4.py
@@ -0,0 +1,8 @@
+name= 'Денис'
+print ('Name:',name)
+age= '44'
+print('Name:',name,'Age:',age)
+age_old= '55'
+print('Name:',name,'Age:',age,'Age_Old:',age_old)
+Is_Student= True
+print('Name:,',name,',Age:,',age,',Age_Old:,',age_old,',Is_Student:,', 'True',)
