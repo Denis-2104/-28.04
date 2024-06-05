36b2a1f1ff3a7482c433f05ddab09e2d0e63e632
Denis-2104 <gavrilinyandex@gmail.com>
2024-06-05 09:41:40 +0200

    Add files via upload

---
+++ homeworke5.py
@@ -0,0 +1,28 @@
+#food=["orange","coconut","coca cola"]
+#print (food [0])
+#food[0]= "voda"
+#print(food [0:2:2])
+#tuple= 1,2,3,4,True, "String"
+#list= [1,2,3,4,True, "String"]
+#tuple2= (1,2,3,4,)
+#tuple3= [1,2,3,4,]
+#print (tuple)
+#print(tuple2)
+#print(tuple3)
+#tuple[0] = 200
+#print(tuple.__sizeof__())
+#print(list.__sizeof__())
+#tuple=([1,2,3], 0) + (1,2)
+#print(tuple)
+#tuple [0][0]= 10
+#print(tuple)
+#tuple= (1,2,3) * 2
+#print (tuple)
+immutable_var=(23,45,55)
+print(immutable_var)
+immutable_var=[0][0]=3
+print(immutable_var)
+mutable_list= ' Привет', 'мир' ,+1,2,3, True
+#mutable_list=(Привет, мир)+(1,2,3)
+#mutable_list=(Привет, мир)+(1,2,3)
+print(mutable_list)
