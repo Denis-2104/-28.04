bdac48987cf0d8e7f25a89e0fd6c760d6d796c18
Denis-2104 <gavrilinyandex@gmail.com>
2024-06-08 21:16:12 +0200

    Add files via upload
    Я так и не понял условия задачи, в чате мне не помогли, сделал на свое усмотрение.

---
+++ homework7.py
@@ -0,0 +1,43 @@
+#print(1)
+#print(2)
+#print(3)
+#name =input('Введите ваше имя:')
+#if name == 'Urban': # При вводе в консоле имя Urban выводится привет Urban принт выводится с 4 пробелами 
+#    print('Здравствуйте администратор')
+#if name == 'Денис':
+#    print('Здравствуйте преподователь')
+#else: # Если не сработал if  пользуйся else 
+#    print ("Привет ", name)
+#number=int(input('Введите число:')) #Fizz если число /3 Buzz / 5 Fizz Buzz и 3 и 5 
+#if number % 3 == 0 and number % 5 == 0:
+#    print('Fizz Buzz')
+#elif number % 3 == 0:  # if когда создаем блоки условий, elif не может быть первой строчкой.
+#    print('Fizz')
+#elif number % 5 == 0:
+#    print  ('Buzz')
+#else:
+#    print    ('Число не подходит')
+#or (Не строгий оператор ) или and (строгий оператор )
+#number=int(input('Введите число:')) #first . second . third
+#print('number')
+#if number / 20 == 0 and number / 2 == 0:
+#    print('first')
+
+
+fist = int(input("Введите базовое целое число: "))
+second = int(input("Введите следующее целое число: "))
+
+if fist > second:
+  print("more ok")
+
+fist = int(input("Введите целое число, меньшее, чем базовое: "))
+
+if fist < second:
+ print("less ok")
+else:
+  print("error")
+
+#first
+#second
+#third
+
