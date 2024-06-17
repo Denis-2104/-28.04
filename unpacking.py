e0a43e9e673304787ed456331db61e4c468f7b1a
Denis-2104 <gavrilinyandex@gmail.com>
2024-06-17 13:58:55 +0200

    Add files via upload

---
+++ unpacking.py
@@ -0,0 +1,11 @@
+def print_params(a=1, b="строка", c=True):
+    print(a, b, c)
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+values_list = [ 5, True, 'Denis' ]
+values_dict = {'a' : 5, 'b' : True, 'c' : 'Anton'}
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
